7c1e079708ad994b8fc4e2ee0afd30739768af8a
shraddha14902@gmail.com <shraddha14902@gmail.com>
2026-03-13 15:13:58 +0530

    script.js added

---
+++ login_authentication/script.js
@@ -0,0 +1,35 @@
+function register(){
+
+let username=document.getElementById("userName").value;
+let password=document.getElementById("userPass").value;
+
+localStorage.setItem("username",username);
+localStorage.setItem("password",password);
+
+document.getElementById("message").innerText="Registration Successful";
+
+}
+
+
+function login(){
+
+let username=document.getElementById("loginUser").value;
+let password=document.getElementById("loginPass").value;
+
+let storedUser=localStorage.getItem("username");
+let storedPass=localStorage.getItem("password");
+
+if(username===storedUser && password===storedPass){
+
+document.getElementById("message").innerText="Login Successful";
+
+
+}
+
+else{
+
+document.getElementById("message").innerText="Invalid Login";
+
+}
+
+}
